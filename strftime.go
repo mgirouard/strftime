37d5edd81eea8ba8e2a138b74c58590b1e93ad99
Mike Girouard <mgirouard@gmail.com>
2020-10-03 18:51:35 -0700

    Implement %C

--- strftime.go
+++ strftime.go
@@ -1,12 +1,11 @@
 package strftime
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 	"time"
 )
-
-// strftime is a global registry of formatter funcs
-var strftime *reg
 
 // Fmt formats a string with strftime
 func Fmt(s string, t time.Time) string {
@@ -25,26 +24,8 @@ func Fmt(s string, t time.Time) string {
 	return strings.Join(o, "")
 }
 
-func init() {
-	strftime = &reg{
-		fns: map[string]func(time.Time) string{
-			"%%": func(t time.Time) string { return "%" },
-			"%a": func(t time.Time) string { return t.Format("Mon") },
-			"%A": func(t time.Time) string { return t.Format("Monday") },
-			"%b": func(t time.Time) string { return t.Format("Feb") },
-			"%B": func(t time.Time) string { return t.Format("February") },
-			"%c": func(t time.Time) string { return Fmt("%a %b %e %H:%M:%S %Y", t) },
-			"%C": func(t time.Time) string { return t.Format("06") },
-			"%d": func(t time.Time) string { return t.Format("02") },
-			"%D": func(t time.Time) string { return Fmt("%m/%d/%y", t) },
-			"%e": func(t time.Time) string { return Fmt(" 2", t) },
-
-			"%m": func(t time.Time) string { return t.Format("01") },
-			"%Y": func(t time.Time) string { return t.Format("2006") },
-			"%y": func(t time.Time) string { return t.Format("06") },
-		},
-	}
-}
+// strftime is a global registry of formatter funcs
+var strftime *reg
 
 // reg is a registry of format funcs
 type reg struct {
@@ -57,4 +38,44 @@ func (r *reg) get(f string, t time.Time) string {
 		return r.fns[f](t)
 	}
 	return f
+}
+
+func init() {
+	strftime = &reg{
+		fns: map[string]func(time.Time) string{
+			// The abbreviated name of the day of the week
+			"%a": func(t time.Time) string { return t.Format("Mon") },
+
+			// The full name of the day of the week
+			"%A": func(t time.Time) string { return t.Format("Monday") },
+
+			// The abbreviated month name
+			"%b": func(t time.Time) string { return t.Format("Feb") },
+
+			// The full month name
+			"%B": func(t time.Time) string { return t.Format("February") },
+
+			// The preferred date and time representation
+			"%c": func(t time.Time) string { return Fmt("%a %b %e %H:%M:%S %Y", t) },
+
+			// The century number (year/100) as a 2-digit integer
+			"%C": func(t time.Time) string {
+				y, err := strconv.Atoi(Fmt("%Y", t))
+				if err != nil {
+					y = 0
+				}
+				return fmt.Sprintf("%d", y/100)
+			},
+			"%d": func(t time.Time) string { return t.Format("02") },
+			"%D": func(t time.Time) string { return Fmt("%m/%d/%y", t) },
+			"%e": func(t time.Time) string { return Fmt(" 2", t) },
+
+			"%m": func(t time.Time) string { return t.Format("01") },
+			"%Y": func(t time.Time) string { return t.Format("2006") },
+			"%y": func(t time.Time) string { return t.Format("06") },
+
+			// A literal '%' character.
+			"%%": func(t time.Time) string { return "%" },
+		},
+	}
 }
